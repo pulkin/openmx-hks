3ffc261093c1018c32a9adaeecc9e632be630bdb
Artem <gpulkin@gmail.com>
2019-11-21 17:49:44 +0100

    Fix name

--- src/simplejson.c
+++ src/simplejson.c
@@ -23,11 +23,11 @@ void close_json(void *f) {fclose(f);}
 void write_json_header(void *f) {indent = 0; comma = 0; JSON_O_S;}
 void write_json_footer(void *f) {JSON_O_E;}
 void write_json_double_scalar(void *f, char* name, double* data) {JSON_S_KEY(name); JSON_D(*data);}
-void write_json_int_1D_array(void *f, char* name, int* data, int n, int step) {
+void write_json_int_1D_array(void *f, char* name, int* data, int n, int stride) {
     int i=0;
     JSON_S_KEY(name);
     JSON_A_S;
-    for (i=0; i<n; i++) {JSON_I(data[step*i]);}
+    for (i=0; i<n; i++) {JSON_I(data[stride*i]);}
     JSON_A_E;
 }
 void write_json_int_2D_array(void *f, char* name, int* data, int n, int m) {
